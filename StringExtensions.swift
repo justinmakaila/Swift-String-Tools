caa101862fd833497a47fa2a05f2512105105479
justinmakaila <justinmakaila@gmail.com>
2014-09-04 14:24:09 -0400

    Complete comment reformat, added functions to simplify regex searches. Added NSString extensions.

--- StringExtensions.swift
+++ StringExtensions.swift
@@ -24,34 +24,58 @@
 
 import Foundation
 
+// MARK:
+// MARK: String Extensions
+// MARK:
+
 extension String {
     
-    //MARK: helper methods
+    // MARK: Variables
     
     /**
-    Returns the length of the string.
-    
-    :returns: Int length of the string.
-    */
-
+     *  The length of the string.
+     */
     var length: Int {
         return countElements(self)
     }
     
+    /**
+     *  The UTF16 length of the string.
+     */
     var objcLength: Int {
+        // ???: Would it be easier/different to cast to NSString and use .length?
         return self.utf16Count
     }
     
-    //MARK: Linguistics
+    /**
+     *  Indicates if the string is an Email via NSDataDetector
+     */
+    var isEmail: Bool {
+        let dataDetector = NSDataDetector(types: NSTextCheckingType.Link.toRaw(), error: nil),
+            firstMatch = dataDetector.firstMatchInString(self, options: NSMatchingOptions.ReportCompletion, range: NSMakeRange(0, self.length))
+            
+            return (firstMatch?.range.location != NSNotFound && firstMatch?.URL?.scheme == "mailto")
+    }
     
     /**
-    Returns the langauge of a String
-    
-    NOTE: String has to be at least 4 characters, otherwise the method will return nil.
-    
-    :returns: String! Returns a string representing the langague of the string (e.g. en, fr, or und for undefined).
+    *  Converts self to Float.
+    *
+    *  @return Float representation of self.
     */
-    func detectLanguage() -> String! {
+    var floatValue: Float {
+        return (self as NSString).floatValue
+    }
+    
+    // MARK: Utilities
+    // MARK: Linguistics
+    
+    /**
+     *  Returns the langauge of a String
+     *  @discussion: String has to be at least 4 characters, otherwise the method will return nil.
+     *
+     *  @return: String? representing the langague of the string (e.g. en, fr, or und for undefined).
+     */
+    func detectLanguage() -> String? {
         if self.length > 4 {
             var token : dispatch_once_t = 0
             var tagger : NSLinguisticTagger?
@@ -63,50 +87,38 @@ extension String {
             
             return tagger?.tagAtIndex(0, scheme: NSLinguisticTagSchemeLanguage, tokenRange: nil, sentenceRange: nil)
         }
-            return nil
+        
+        return nil
     }
     
     /**
-    Check the text direction of a given String.
-    
-    NOTE: String has to be at least 4 characters, otherwise the method will return false.
-    
-    :returns: Bool The Bool will return true if the string was writting in a right to left langague (e.g. Arabic, Hebrew)
-    
-    */
+     *  Check the text direction of a given String.
+     *  @discussion: String has to be at least 4 characters, otherwise the method will return false.
+     *
+     *  @return: Bool true if the string was writting in a right to left langague (e.g. Arabic, Hebrew)
+     */
     func isRightToLeft() -> Bool {
         let language = self.detectLanguage()
         return (language? == "ar" || language? == "he")
     }
     
-    //MARK: - Usablity & Social
-    
     /**
-    Check that a String is only made of white spaces, and new line characters.
-    
-    :returns: Bool
-    */
+     *
+     *  Check that a String is only made of white spaces, and new line characters.
+     *
+     *  @return Bool indicating if the string is empty characters.
+     */
     func isOnlyEmptySpacesAndNewLineCharacters() -> Bool {
         return self.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()).length == 0
     }
     
-    /**
-    Checks if a string is a valid email address using NSDataDetector.
-    
-    :returns: Bool
-    */
-    var isEmail: Bool {
-        let dataDetector = NSDataDetector(types: NSTextCheckingType.Link.toRaw(), error: nil),
-            firstMatch = dataDetector.firstMatchInString(self, options: NSMatchingOptions.ReportCompletion, range: NSMakeRange(0, self.length))
-            
-        return (firstMatch?.range.location != NSNotFound && firstMatch?.URL?.scheme == "mailto")
-    }
+    // MARK: Social
     
     /**
-    Check that a String is 'tweetable' can be used in a tweet.
-    
-    :returns: Bool
-    */
+     *  Check that a String is 'tweetable' can be used in a tweet.
+     *
+     *  @return Bool indicating if self is tweetable.
+     */
     func isTweetable() -> Bool {
         let tweetLength = 140,
             // ???: Why multiply by 23?
@@ -121,31 +133,14 @@ extension String {
     }
     
     /**
-    Gets an array of Strings for all links found in a String
-    
-    :returns: [String!]
-    */
-    func getLinks() -> [String] {
-        let error: NSErrorPointer = NSErrorPointer(),
-            detector = NSDataDetector(types: NSTextCheckingType.Link.toRaw(), error: error),
-            links = detector.matchesInString(self, options: NSMatchingOptions.WithTransparentBounds, range: NSMakeRange(0, self.utf16Count)) as [NSTextCheckingResult]
-    
-        return links.filter { link in
-            return link.URL != nil
-        }.map { link -> String in
-            return link.URL!.absoluteString!
-        }
-    }
-    
-    /**
-    Gets an array of URLs for all links found in a String
-    
-    :returns: [NSURL]
+    *  Gets an array of URLs for all links found in self.
+    *
+    *  @return [NSURL] representing the URLs found in self
     */
     func getURLs() -> [NSURL] {
         let error: NSErrorPointer = NSErrorPointer(),
-            detector: NSDataDetector = NSDataDetector(types: NSTextCheckingType.Link.toRaw(), error: error),
-            links = detector.matchesInString(self, options: NSMatchingOptions.WithTransparentBounds, range: NSMakeRange(0, self.utf16Count)) as [NSTextCheckingResult]
+        detector: NSDataDetector = NSDataDetector(types: NSTextCheckingType.Link.toRaw(), error: error),
+        links = detector.matchesInString(self, options: NSMatchingOptions.WithTransparentBounds, range: NSMakeRange(0, self.utf16Count)) as [NSTextCheckingResult]
         
         return links.filter { link in
             return link.URL != nil
@@ -154,12 +149,25 @@ extension String {
         }
     }
     
+    /**
+     *  Gets an array of Strings for all links found in self.
+     *
+     *  @discussion Uses getURLs() internally, returning the absoluteString
+     *              of each URL.
+     *
+     *  @return [String] representing the links found in self
+     */
+    func getLinks() -> [String] {
+        return getURLs().map { url -> String in
+            return url.absoluteString!
+        }
+    }
     
     /**
-    Gets an array of dates for all dates found in a String
-    
-    :returns: [NSDate]
-    */
+     *  Gets an array of dates for all dates found in self.
+     *
+     *  @return [NSDate]
+     */
     func getDates() -> [NSDate] {
         let error: NSErrorPointer = NSErrorPointer(),
             detector: NSDataDetector = NSDataDetector(types: NSTextCheckingType.Date.toRaw(), error: error),
@@ -173,112 +181,174 @@ extension String {
     }
     
     /**
-    Gets an array of strings (hashtags #acme) for all links found in a String
+     *  Gets an array of hashtags found in self.
+     *
+     *  @param includeDevice a Bool indicating whether or not the hashtag device (#) should
+     *          be included in the results. Default is true.
+     *
+     *  @discussion Returns an empty array if there are no matches.
+     *
+     *  @return [String] of substrings matching the regex #(\w+).
+     */
+    func getHashtags(includeDevice: Bool = true) -> [String] {
+        return self.getMatchesForPrefix("#", includePrefix: includeDevice)
+    }
     
-    :returns: [String]
-    */
-    func getHashtags() -> [String] {
-        let hashtagDetector = NSRegularExpression(pattern: "#(\\w+)", options: NSRegularExpressionOptions.CaseInsensitive, error: nil),
-            results = hashtagDetector.matchesInString(self, options: NSMatchingOptions.WithoutAnchoringBounds, range: NSMakeRange(0, self.utf16Count)) as [NSTextCheckingResult]
-
-        return results.map { textCheckingResult -> String in
-            return self[textCheckingResult.rangeAtIndex(0)]
+    /**
+     *  Gets an array of unique hashtags found in self.
+     *
+     *  @param includeDevice a Bool indicating whether or not the hashtag device (#) should
+     *         be included in the results. Default is true.
+     *
+     *  @return [String] of unique substrings matching the regex #(\w+).
+     */
+    func getUniqueHashtags(includeDevice: Bool = true) -> [String] {
+        return NSSet(array: self.getHashtags(includeDevice: includeDevice)).allObjects as [String]
+    }
+    
+    /**
+     *  Gets an array of mentions found in self.
+     *
+     *  @param includeDevice a Bool indicating whether or not the mention device (@) should
+     *         be included in the results. Default is true.
+     *
+     *  @discussion Returns an empty array if no matches are found.
+     *
+     *  @return [String] of substrings matching the regex @(\w+).
+     */
+    func getMentions(includeDevice: Bool = true) -> [String] {
+        return self.getMatchesForPrefix("@", includePrefix: includeDevice)
+    }
+    
+    /**
+     *  Gets an array of unique mentions found in self.
+     *
+     *  @param includeDevice a Bool indicating whether or not the mention device (@) should
+     *         be included in the results. Default is true.
+     *
+     *  @return [String] of unique substrings matching the regex @(\w+).
+     */
+    func getUniqueMentions(includeDevice: Bool = true) -> [String] {
+        return NSSet(array: self.getMentions(includeDevice: includeDevice)).allObjects as [String]
+    }
+    
+    /**
+     *  Gets an array of strings with the prefix.
+     *
+     *  @param prefix The prefix to search for.
+     *  @param includePrefix Bool indicating whether or not the prefix should be included
+     *         in the results. Default is true.
+     *
+     *  @discussion Internally uses getMatchesForRegex(...). Returns an empty array if no
+     *              matches are found.
+     *
+     *  @return [String] of substrings matching the regex "`prefix`(\w+)".
+     */
+    func getMatchesForPrefix(prefix: String, includePrefix: Bool = true) -> [String] {
+        if let results = self.getMatchesForRegex("\(prefix)(\\w+)") {
+            if includePrefix == true {
+                return results
+            } else {
+                return results.map { result -> String in
+                    return result[1..<result.length]
+                }
+            }
         }
-    }
-    
-    /**
-    Gets an array of distinct strings (hashtags #acme) for all hashtags found in a String
-    
-    :returns: [String]
-    */
-    func getUniqueHashtags() -> [String] {
-        return NSSet(array: self.getHashtags()).allObjects as [String]
-    }
-    
-    
-    /**
-    Gets an array of strings (mentions @apple) for all mentions found in a String
-    
-    :returns: [String]
-    */
-    func getMentions() -> [String] {
-        let mentionDetector = NSRegularExpression(pattern: "@(\\w+)", options: NSRegularExpressionOptions.CaseInsensitive, error: nil),
-            results = mentionDetector.matchesInString(self, options: NSMatchingOptions.WithoutAnchoringBounds, range: NSMakeRange(0, self.utf16Count)) as [NSTextCheckingResult]
         
-        return results.map { textCheckingResult -> String in
-            return self[textCheckingResult.rangeAtIndex(0)]
-        }
+        return [String]()
     }
     
     /**
-    Check if a String contains a Date in it.
+     *  Gets an array of strings matching `regex` found in self.
+     *
+     *  @param regex The regex pattern to use for matching.
+     *  @param options The NSRegularExpressionOptions to be used for matching.
+     *                 Default is NSRegularExpressionOptions.CaseInsensitive.
+     *
+     *  @discussion If `regex` is not a valid regular expression pattern, this will return nil.
+     *
+     *  @return [String]? an array of String objects matching `regex`
+     */
+    func getMatchesForRegex(regex: String, options: NSRegularExpressionOptions = .CaseInsensitive) -> [String]? {
+        var error: NSError?
+        let regularExpression = NSRegularExpression(pattern: regex, options: options, error: &error)
+        
+        if error == nil {
+            if let results = regularExpression.matchesInString(self, options: NSMatchingOptions.WithoutAnchoringBounds, range: NSMakeRange(0, self.utf16Count)) as? [NSTextCheckingResult] {
+                return results.map { textCheckingResult -> String in
+                    return self[textCheckingResult.rangeAtIndex(0)]
+                }
+            }
+        }
+        
+        return nil
+    }
     
-    :returns: Bool with true value if it does
-    */
+    /**
+     *  Checks if self contains a date.
+     *
+     *  @return Bool true if there is a date present, else false.
+     */
     func containsDate() -> Bool {
         return self.getDates().count > 0
     }
     
-    
     /**
-    Check if a String contains a link in it.
-    
-    :returns: Bool with true value if it does
-    */
+     *  Check if a String contains a link in it.
+     *
+     *  @return Bool true if there is a link present, else false.
+     */
     func containsLink() -> Bool {
         return self.getLinks().count > 0
     }
 
     /**
-    :returns: Base64 encoded string
-    */
-    func encodeToBase64Encoding() -> String {
-        let utf8str = self.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)!,
-            base64EncodedString = utf8str.base64EncodedStringWithOptions(NSDataBase64EncodingOptions.fromRaw(0)!)
+     *  Encodes self to Base64.
+     *
+     *  @return String self encoded to Base64.
+     */
+    func encodeBase64() -> String {
+        let utf8str = self.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)!
         
-        return base64EncodedString
+        return utf8str.base64EncodedStringWithOptions(NSDataBase64EncodingOptions.fromRaw(0)!)
     }
     
     /**
-    :returns: Decoded Base64 string
-    */
-    func decodeFromBase64Encoding() -> String {
-        let base64data = NSData(base64EncodedString: self, options: NSDataBase64DecodingOptions.fromRaw(0)!),
-            decodedString = NSString(data: base64data, encoding: NSUTF8StringEncoding)
+     *  Decodes self from Base64.
+     *
+     *  @return String self decoded from Base64
+     */
+    func decodeBase64() -> String {
+        let base64data = NSData(base64EncodedString: self, options: NSDataBase64DecodingOptions.fromRaw(0)!)
         
-        return decodedString
+        return NSString(data: base64data, encoding: NSUTF8StringEncoding)
     }
+}
 
+// MARK: Subscript Methods
+
+extension String {
     /**
-        Float value from a string
-    */
-    var floatValue: Float {
-        return (self as NSString).floatValue
-    }
-    
-    // MARK: Subscript Methods
-    
-    /**
-     *  Allows subscript access to characters in a String
+     *  Allows subscript access to characters in self
      *  by integers.
      */
-    subscript (i: Int) -> String {
-        return String(Array(self)[i])
+    subscript (index: Int) -> String {
+        return String(Array(self)[index])
     }
     
     /**
-     *  Allows subscript access to characters in a String
+     *  Allows subscript access to characters in self
      *  using integer Ranges.
      */
-    subscript (r: Range<Int>) -> String {
-        var start = advance(startIndex, r.startIndex),
-            end = advance(startIndex, r.endIndex)
+    subscript (range: Range<Int>) -> String {
+        var start = advance(startIndex, range.startIndex),
+            end = advance(startIndex, range.endIndex)
             
         return substringWithRange(Range(start: start, end: end))
     }
     
     /**
-     *  Allows subscript access to characters in a String
+     *  Allows subscript access to characters in self
      *  using NSRange.
      */
     subscript (range: NSRange) -> String {
@@ -287,7 +357,7 @@ extension String {
     }
     
     /**
-     *  Allows subscript access to characters in a String
+     *  Allows subscript access to characters in self
      *  using a String parameter to represent a substring.
      */
     subscript (substring: String) -> Range<String.Index>? {
@@ -295,8 +365,26 @@ extension String {
     }
 }
 
+// MARK:
+// MARK:  NSString Extensions
+// MARK:
+
+// MARK: Subscript Methods
+
 extension NSString {
+    /**
+     *  Allows subscript access to characters in self
+     *  using a String parameter to represent a substring.
+     */
     subscript (substring: String) -> NSRange? {
         return rangeOfString(substring)
+    }
+    
+    /**
+     *  Allows subscript access to characters in self
+     *  using a NSString parameter to represent a substring.
+     */
+    subscript (substring: NSString) -> NSRange? {
+        return self[(substring as String)]
     }
 }
